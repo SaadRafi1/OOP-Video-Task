d4dac18a377ffe57f8ebd4b3a8ea12ac7204f6fa
SaadRafi1 <saadrafi990@gmail.com>
2024-03-26 15:59:49 +0500

    Mosh Hamdani video task

---
+++ StopWatch index.js
@@ -0,0 +1,29 @@
+function Stopwatch() {
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
+  this.start = function () {
+    if (running) throw new Error("Stop wath has already started.");
+    running = true;
+    startTime = new Date();
+  };
+  this.stop = function () {
+    if (!running) throw new Error("Stopwatch is not started");
+    running = false;
+    endTime = new Date();
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    duration += seconds;
+  };
+  this.start = function () {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
+  object.defineProperty(this, "duration", {
+    get: function () {
+      return duration;
+    },
+  });
+}
